1792c693ff92612d918b095ced5ad8daa9ddbc80
byeolee1221 <zicsso1221@gmail.com>
2024-01-05 18:50:11 +0900

    코드를 좀 더 실용적으로 변환하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
 
-  let post = "강남 우동 맛집";
   const postName = [
     "여자 코트 추천",
     "강남 우동 맛집",
@@ -12,8 +10,9 @@ function App() {
   ];
 
   const [name, setName] = useState(postName);
-  const [like, setLike] = useState(0);
+  const [like, setLike] = useState([0, 0, 0]);
   const [modal, setModal] = useState(false);
+  const [title, setTitle] = useState(0);
 
   const nameChangeHandler = () => {
     let postNameCopy = [...postName];
@@ -21,8 +20,10 @@ function App() {
     setName(postNameCopy);
   }
 
-  const likeChangeHandler = () => {
-    setLike(like + 1);
+  const likeChangeHandler = (num) => {
+    const likeNumCopy = [...like];
+    likeNumCopy[num]++;
+    setLike(likeNumCopy);
   }
 
   const sortChangeHandler = () => {
@@ -39,12 +40,13 @@ function App() {
     }
   }
 
-  const Modal = () => {
+  const Modal = (props) => {
     return (
       <div className="modal">
-        <h4>제목</h4>
+        <h4>{props.name}</h4>
         <p>날짜</p>
         <p>상세내용</p>
+        <button onClick={props.nameChange}>글 수정</button>
       </div>
     );
   }
@@ -62,22 +64,24 @@ function App() {
       <div className="black-nav">
         <h4>React Blog</h4>
       </div>
-      <div className="list">
-        <h4>{name[0]} <span onClick={likeChangeHandler} className="likeSpan">👍</span> {like} </h4>
-        <p>1월 4일 발행</p>
-        <button onClick={nameChangeHandler}>성별 변경</button>
-        <button onClick={sortChangeHandler}>가나다순 정렬</button>
-      </div>
-      <div className="list">
-        <h4>{name[1]}</h4>
-        <p>1월 4일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{name[2]}</h4>
-        <p>1월 4일 발행</p>
-      </div>
+      {name.map((data, i) => {
+        return (
+          <div className="list" key={i}>
+            <h4 className="post" onClick={() => {modalHandler(i); setTitle(i)}}>
+              {data}
+              <span onClick={likeChangeHandler} className="likeSpan">
+                👍
+              </span>
+              {like[i]}
+            </h4>
+            <p>1월 4일 발행</p>
+            <button onClick={nameChangeHandler}>성별 변경</button>
+            <button onClick={sortChangeHandler}>가나다순 정렬</button>
+          </div>
+        );
+      })}
       <button onClick={modalHandler}>모달</button>
-      {modal ? <Modal /> : null}
+      {modal ? <Modal name={name[title]} nameChange={nameChangeHandler} /> : null}
       <Apple />
     </div>
   );
